7c18cf411e2a86b1c6c7354367069ab795e14a19
Abirhanned <hannedabir@gmail.com>
2024-12-22 14:54:25 +0100

    Update acceuilAppli.php

--- acceuilAppli.php
+++ acceuilAppli.php
@@ -1,17 +1,29 @@
+<?php
+session_start();
+
+// Vérifiez si l'utilisateur est connecté, sinon redirigez vers la page de connexion
+if (!isset($_SESSION['user_login'])) {
+    header('Location: identification.php');
+    exit();
+}
+?>
+
+
+
 
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-	<title>Stage BTS</title>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" href="style.css">
+    <title>Stage BTS</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-	<header>
-		
-	</header>
- 
+    <h1>Bienvenue, <?= $_SESSION['user_login']; ?>!</h1>
+    <p>Vous êtes connecté(e) en tant que <?= $_SESSION['user_role']; ?>.</p>
+   
+
 	<div class="contenu">
 		<nav class="nav flex-column">
 			<a href="acceuilAppli.php" class="clic">  <img src="icons/home.png" alt="logo" width="30" height="24"  >
